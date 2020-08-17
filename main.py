668a92564267bebb969a2674fbea14e521551f32
Arnold Wildt <arnoldwildt@gmail.com>
2020-08-17 21:51:50 +0200

    argparse, functions to main added

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
 import argparse
+from datetime import date
+
 from expr_functions import build_expr
 from image_plot import plot_image
-import time
 
 
-# red_expr = build_expr(.97)
-# green_expr = build_expr(.97)
-# blue_expr = build_expr(.97)
-# print("red = " + str(red_expr))
-# print("green = " + str(green_expr))
-# print("blue = " + str(blue_expr))
-# image = plot_image(red_expr, green_expr, blue_expr, 100)
+def save_expr_file(saved_strings):
+    with open(f"Expr_strings_{date.today()}.txt", "w") as save_file:
+        for i, saved_string in enumerate(saved_strings):
+            r, g, b = saved_string
+            img_seg = f"image{i}\nred: {r}\ngreen: {g}\nblue: {b}\n\n"
+            save_file.writelines(img_seg)
 
-# red_str = "math.sin(math.pi * quotient(pow(parabola(avg(0.17950211560429652, y * math.pi) * math.pi)), -1))"
-# green_str = "(sqrt(pow(math.sin(0))) * -1)"
-# blue_str = "parabola(mix(parabola(dip(y)), quotient(modulo(0, 1), dip(quotient(1, 0))), sqrt(level(1, quotient(mix(1 * math.pi, y, x), avg(0.3458170417828812, 0.1633586385516358)), level(0, 0.546544911177rred_expr, 0.23597017071203397), 0.5231512601863461)green_exprx = "math.sin(math.pi * math.sin(math.pi *blue_exprin(math.pi * (math.sin(math.pi * math.sin(math.pi * math.sin(math.pi * math.sin(math.pi * math.blue_canvasi * y))))) * math.cos(math.pi * math.sin(math.pi * math.cos(math.pi * avg(math.sin(math.pi * y), (x * x)))))))))"
-x = "math.sin(math.pi * math.sin(math.pi * math.sin(math.pi * (math.sin(math.pi * math.sin(math.pi * math.sin(math.pi * math.sin(math.pi * math.cos(math.pi * y))))) * math.cos(math.pi * math.sin(math.pi * math.cos(math.pi * avg(math.sin(math.pi * y), (x * x)))))))))"
-start = time.time()
-image = plot_image(x, x, x, 300, is_str=True)
-print(f"Took {time.time() - start}s")
-# image = plot_image(red_str, green_str, blue_str, 300, is_str=True)
 
-image.show()
+def create_images(num=1, prob=0.97, size=150, file_path=None):
+    if file_path:
+        pass
+
+    saved_strings = []
+
+    for i in range(num):
+        red_expr = build_expr(prob)
+        green_expr = build_expr(prob)
+        blue_expr = build_expr(prob)
+
+        image = plot_image(red_expr, green_expr, blue_expr, size)
+        image.save(f"./images/image{i}.png")
+        saved_strings.append([red_expr, green_expr, blue_expr])
+
+    save_expr_file(saved_strings)
+
 
 if __name__ == "__main__":
-    pass
+
+    parser = argparse.ArgumentParser(
+        description="Creates random art")
+    parser.add_argument("--num", metavar="Number of images",
+                        type=int, default=1)
+    parser.add_argument("--prob", metavar="Probability",
+                        type=float, default=0.99)
+    parser.add_argument("--size", metavar="Size of image",
+                        type=int, default=150)
+    parser.add_argument("--path", metavar="Expression file path", type=str)
+    args = parser.parse_args()
+
+    create_images(*vars(args).values())
