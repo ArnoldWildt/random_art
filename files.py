1078582316308c796720bc04fd31075502806389
Arnold Wildt <arnoldwildt@gmail.com>
2020-08-23 00:53:55 +0200

    moved file functions to diffrent file

---
+++ files.py
@@ -0,0 +1,26 @@
+from datetime import date
+
+
+def save_expr_file(saved_strings):
+    with open(f"Expr_strings_{date.today()}.txt", "w") as save_file:
+        for i, saved_string in enumerate(saved_strings):
+            r, g, b = saved_string
+            img_seg = f"image{i}\nred: {r}\ngreen: {g}\nblue: {b}\n\n"
+            save_file.writelines(img_seg)
+
+
+def read_file(path):
+    with open(path) as expr_file:
+        lines = expr_file.readlines()
+
+    # Filters Lines with image and blank out
+    def filter_func(x):
+        return True if not x == "\n" and "image" not in x else False
+    lines = list(filter(filter_func, lines))
+
+    # Removes "red: ", "green: ", "blue: " from the start of the line
+    txts_dict = {"r": 5, "g": 7, "b": 6}
+    for i, line in enumerate(lines):
+        lines[i] = line[txts_dict[line[0]]:]
+
+    return lines
