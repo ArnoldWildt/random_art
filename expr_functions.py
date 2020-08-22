bc2da4027b70a09b6cc58b66df211a57a0d2a997
Arnold Wildt <arnoldwildt@gmail.com>
2020-08-23 00:56:22 +0200

    naming two not suited -> multi

--- expr_functions.py
+++ expr_functions.py
@@ -2,10 +2,10 @@ from inspect import getmembers, isclass
 import random
 import none_input_expr
 import one_output_expr
-import two_output_expr
+import multi_output_expr
 
 end_operators = [op[1] for op in getmembers(none_input_expr, isclass)]
-norm_operators = [op[1] for module in [one_output_expr, two_output_expr]
+norm_operators = [op[1] for module in [one_output_expr, multi_output_expr]
                   for op in getmembers(module, isclass)]
 
 
